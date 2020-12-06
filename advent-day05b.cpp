83a0c12c17815b6db8a5a17a7d569721a85e5d89
Adrian <blackhatadrian@gmail.com>
2020-12-05 18:08:43 -0700

    Day 5

---
+++ advent-day05b.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    std::string seatline;
+
+    short int row = 0;
+    short int seat = 0;
+    short int bit = 0;
+    std::vector<int> seatids;
+    int biggest = 0;
+    while (std::getline(std::cin, seatline))
+    {
+        if (seatline.length() > 7)
+        {
+            row = 0;
+            seat = 0;
+            for (int f = 0; f < 7; f++)
+            {
+                if (seatline[f] == 'F')
+                {
+                    bit = 0 << (6-f);
+                }
+                else
+                {
+                    bit = 1 << (6-f);
+                }
+                row |= bit;
+            }
+            for (int f = 7; f < 10; f++)
+            {
+                if (seatline[f] == 'L')
+                {
+                    bit = 0 << (3-f + 6);
+                }
+                else
+                {
+                    bit = 1 << (3-f + 6);
+                }
+                seat |= bit;
+            }
+            int seatid = (8 * row ) + seat;
+            if (biggest < seatid)
+                biggest = seatid;
+            cout << "Seat ID: " << seatid << endl;
+        }
+
+    }
+    cout << "Biggest ID: " << biggest << endl;
+
+    return 0;
+}
