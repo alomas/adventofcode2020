cc5c56756af065540f409e57c9b741a4d8ddca11
Adrian <blackhatadrian@gmail.com>
2020-12-08 08:52:04 -0700

    Day 7 stuff.

--- advent-day07a.cpp
+++ advent-day07a.cpp
@@ -7,6 +7,41 @@
 
 using namespace std;
 
+int findGold(map<string, map<string, int>> &outerbagmap, string bagtosearch, int depth)
+{
+    bool foundgold = false;
+    int goldcount = 0;
+    int result = 0;
+    for (int f=0; f<depth; f++)
+        cout << "\t";
+    cout  << "Searching " << bagtosearch << endl;
+    auto item = outerbagmap.find(bagtosearch);
+    if (item != outerbagmap.end())
+    {
+        for_each(item->second.begin(), item->second.end(), [&outerbagmap, &result, depth](const std::pair<string, int> subitem)
+        {
+            for (int f=0; f<depth+1; f++)
+                cout << "\t";
+            cout  << subitem.first << endl;
+            if (subitem.first == "shiny gold bag")
+                return 1;
+           if (subitem.first != "no other bag")
+           {
+               result = findGold(outerbagmap, subitem.first, depth + 1);
+               for (int f=0; f<depth+1; f++)
+                   cout << "\t";
+               if (result > 0)
+               {
+                   cout << "Found Gold!" << endl;
+                   return 1;
+               }
+           }
+        });
+    }
+    if (result > 0)
+        return 1;
+}
+
 int main(int argc, char *argv[]) {
     std::string qline;
     int total=0;
@@ -18,20 +53,22 @@ int main(int argc, char *argv[]) {
 
     while (std::getline(std::cin, qline) && qline!="")
     {
-        cout << "Processing line: "  << qline << endl;
+        //cout << "Processing line: "  << qline << endl;
         int outerPos = qline.find(" contain ");
         string outsidebag = qline.substr(0, outerPos);
         if (outsidebag[outsidebag.length()-1] == 's')
         {
             outsidebag.erase(outsidebag.length()-1);
         }
-        cout << outsidebag << " <- ";
+        //cout << outsidebag << " <- ";
         int innerPos = qline.find(", ", outerPos);
         int middle = strlen(" contain ");
         middle += outerPos;
-        outerbagmap.clear();
-        string innerbags = qline.substr(middle, innerPos);
+        bool lastone = false;
+        string innerbags = qline.substr(middle);
         {
+            innerbagmap.clear();
+
             char c = ',';
             string buff = "";
             vector<string> v;
@@ -41,10 +78,14 @@ int main(int argc, char *argv[]) {
                 if(n != c) buff+=n; else
                 if(n == c && buff != "") {
                     fieldnum++;
-                    cout << " fieldnum: " << fieldnum << endl;
+                    // cout << " fieldnum: " << fieldnum << endl;
                     if (buff[buff.length()-1] == 's')
                     {
                         buff.erase(buff.length()-1);
+                    }
+                    if (buff[0] == ' ')
+                    {
+                        buff.erase(0, 1);
                     }
                     int num;
                     string bagtype;
@@ -53,28 +94,77 @@ int main(int argc, char *argv[]) {
                     num = stoi(numstr);
                     bagtype = buff.substr(nPos+1);
                     innerbagmap.insert(make_pair(bagtype, num));
+                    // cout << bagtype << ", " << num << endl;
                     v.push_back(buff); buff = ""; }
             }
             if (buff[0] == ' ')
             {
-                buff.erase(0);
+                buff.erase(0, 1);
             }
             if (buff[buff.length()-1] == '.')
             {
+                lastone = true;
                 buff.erase(buff.length()-1);
+                total++;
             }
             if (buff[buff.length()-1] == 's')
             {
                 buff.erase(buff.length()-1);
             }
             if(buff != "") v.push_back(buff);
-            outerbagmap.insert(make_pair(outsidebag, innerbagmap));
-            for(auto n:v) cout << n << endl;
-            innerbagmap.clear();
+            int num;
+            string bagtype;
+            int nPos = buff.find(" ");
+            string numstr = buff.substr(0, nPos);
+            if (buff == "no other bag")
+            {
+                num = 0;
+                bagtype = "no other bag";
+            }
+            else {
+                num = stoi(numstr);
+                bagtype = buff.substr(nPos + 1);
+            }
+            innerbagmap.insert(make_pair(bagtype, num));
+            if (lastone) {
+                outerbagmap.insert(make_pair(outsidebag, innerbagmap));
+                lastone = true;
+            }
+            //for(auto n:v) cout << n << endl;
         }
 
     }
+    for_each(outerbagmap.begin(), outerbagmap.end(), [](const std::pair<string, map<string, int>> item)
+    {
+        cout << "Outer: " << item.first << endl;
+        for_each(item.second.begin(), item.second.end(), [](const std::pair<string, int> subitem)
+        {
+            cout << "\t" << subitem.second << " " << subitem.first << endl;
+        });
+    });
     cout << "Total: " << total << endl;
+
+    string sentinel = "light red bag";
+    findGold(outerbagmap, "light red bag", 1);
+
+    if (argc > 1)
+    {
+        auto item = outerbagmap.find(argv[1]);
+        if (item==outerbagmap.end())
+        {
+            cout << "Could not find outer bag " << argv[1] << endl;
+        }
+        else
+        {
+            map<string, int> theitem;
+            theitem = item->second;
+            cout << "For Outer Bag " << argv[1] << ": " << endl;
+            for_each(item->second.begin(), item->second.end(), [](const std::pair<string, int> subitem)
+            {
+                cout << "\t" << subitem.second << " " << subitem.first << endl;
+            });
+        }
+    }
 
     return 0;
 }
